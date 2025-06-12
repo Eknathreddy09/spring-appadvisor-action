9f810b60c33eb4b844e0ea5dce1a192ebbf07b27
Eknathreddy09 <eknath.reddy09@gmail.com>
2025-06-12 14:39:21 +0530

    Create app-advisor.sh

---
+++ app-advisor.sh
@@ -0,0 +1,9 @@
+VERSION=1.3.1
+curl -L -H "Authorization: Bearer $ARTIFACTORY_TOKEN" -o /tmp/advisor-cli.tar -X GET https://packages.broadcom.com/artifactory/spring-enterprise/com/vmware/tanzu/spring/application-advisor-cli-linux/$VERSION/application-advisor-cli-linux-$VERSION.tar
+tar -xf /tmp/advisor-cli.tar --strip-components=1 -C /tmp
+install /tmp/advisor /usr/local/bin/advisor
+
+advisor build-config get
+advisor build-config publish
+advisor upgrade-plan get
+advisor upgrade-plan apply
