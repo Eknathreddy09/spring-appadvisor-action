99a4d3d5c50e5434eb0427f4ffbb38ff125972c3
Eknathreddy09 <eknath.reddy09@gmail.com>
2025-06-12 19:45:35 +0530

    Update app-advisor.sh

--- app-advisor.sh
+++ app-advisor.sh
@@ -3,6 +3,47 @@ curl -L -H "Authorization: Bearer $ARTIFACTORY_TOKEN" -o /tmp/advisor-cli.tar -X
 tar -xf /tmp/advisor-cli.tar --strip-components=1 -C /tmp
 install /tmp/advisor /usr/local/bin/advisor
 
+cat > /home/runner/.m2/settings.xml << 'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 
+                              http://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <servers>
+    <server>
+      <id>spring-enterprise-subscription</id>
+      <username>eknath.reddy@broadcom.com</username>
+      <password>${secrets.ArtifactoryToken}</password>
+    </server>
+  </servers>
+  <profiles>
+    <profile>
+      <id>spring-enterprise</id>
+      <activation>
+        <activeByDefault>true</activeByDefault>
+      </activation>
+      <repositories>
+        <repository>
+          <id>spring-enterprise-subscription</id>
+          <url>https://packages.broadcom.com/artifactory/spring-enterprise</url>
+        </repository>
+      </repositories>
+      <pluginRepositories>
+        <pluginRepository>
+          <id>spring-enterprise-subscription</id>
+          <url>https://packages.broadcom.com/artifactory/spring-enterprise</url>
+        </pluginRepository>
+      </pluginRepositories>
+    </profile>
+  </profiles>
+
+  <activeProfiles>
+    <activeProfile>spring-enterprise</activeProfile>
+  </activeProfiles>
+
+</settings>
+EOF
+
 java --version
 echo $JAVA_HOME
 advisor --version
